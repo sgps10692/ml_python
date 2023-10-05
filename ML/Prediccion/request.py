e22fa561cac1f67dd7a9b6791c6a5e8123d87ab4
sgps10692 <sgps106922023@gmail.com>
2023-10-05 11:56:11 -0500

    dsds

--- ML/Prediccion/request.py
+++ ML/Prediccion/request.py
@@ -1,47 +1,81 @@
+import requests
+import json
+import time
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import linear_model
 from sklearn.metrics import explained_variance_score
 from sklearn.model_selection import train_test_split
 
+# Cargar el archivo CSV con los datos
 weather_data = pd.read_csv('udca.csv')
 
+# Ordenar los datos por la columna 'Fecha'
 weather_data_ordered = weather_data.sort_values(by='Fecha')
 
-# Reset index to restore its order
+# Restablecer el índice para restaurar su orden
 weather_data_ordered.reset_index(drop=True)
 
-# %% [5] Data Analysis and Interpretation
 
-
-# Compute correlations
-
-# TO DO: Get the correlation for different combinations of variables.
-#       Contrast them with the weather_correlations dataframe
-
-
-# %% [6] Data Modeling and Prediction
-
-# Get data subsets for the model
+# Obtener los datos necesarios para el modelo
 x_train, x_test, y_train, y_test = train_test_split(
     weather_data_ordered['PREACU2MIN'], weather_data_ordered['HUMSUEPROF1MTS'],
     test_size=0.25)
 
-# Run regression
+# Entrenar el modelo de regresión lineal
 regression = linear_model.LinearRegression()
 regression.fit(x_train.values.reshape(-1, 1), y_train.values.reshape(-1, 1))
 
-# Print coefficients
-print(regression.intercept_, regression.coef_)  # beta_0, beta_1
+# Imprimir los coeficientes del modelo
+print("Intercept:", regression.intercept_)
+print("Coeficiente:", regression.coef_)
 
+# Realizar predicciones con el modelo
+y_predict = regression.predict(x_test.values.reshape(-1, 1))
 
-# %% [7] Predictive Model Testing and Evaluation
-# Plot predicted model with test data
-y_predict = regression.predict([[5]])
+print('Predicciones:', y_predict)
 
-print('ghdcjgdhdsgh', y_predict)
+# Evaluar el modelo
+variance_score = explained_variance_score(y_test, y_predict)
+print('Explained Variance Score:', variance_score)
 
-# TODO: Using the model, predict the temperature for a given level of humidity
+# Datos comunes a todas las solicitudes
+parameter_variable_id = 13
+location_id = 1
 
-# Evaluate model numerically
-explained_variance_score(y_test, y_predict)
+# Cabeceras si es necesario
+cabeceras = {
+    'Content-Type': 'application/json',
+}
+
+# Convierte los datos comunes en una cadena JSON una vez
+result_data = {
+    'parameter_variable_id': parameter_variable_id,
+    'location_id': location_id,
+}
+url = "http://127.0.0.1:8000/api/meteorological-values"
+
+dataJson  = json.dumps(result_data)
+
+# Realizar las solicitudes POST para cada predicción en y_predict
+for prediction in y_predict:
+    result_data['value'] = int(prediction[0])  # El resultado de la predicción
+    
+    # Realizar la solicitud POST
+    response = requests.post(url, json=result_data, headers=cabeceras)
+
+    if response.status_code == 200:
+        print('Resultado guardado exitosamente.')
+    else:
+        print(f'Error al guardar el resultado. Código de estado: {response.status_code}')
+
+        # Intenta acceder al contenido de la respuesta para obtener más detalles
+        try:
+            error_message = response.json()
+            print('Detalles del error:', error_message)
+        except ValueError:
+            # Si la respuesta no es JSON, simplemente imprime el contenido de la respuesta
+            print('')
+
+    # Agrega un retraso de, por ejemplo, 1 segundo entre solicitudes
+    time.sleep(1)
